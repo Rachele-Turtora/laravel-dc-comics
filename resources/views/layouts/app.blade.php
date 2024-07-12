fcec33a30d8000282abf27ff54a5f4f11de9ed52
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-12 13:57:53 +0200

    [UPD] configuration

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Template</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    @include('shared.header')
+    <h1>Template Laravel</h1>
+    @include('shared.footer')
+</body>
+
+</html>
---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <p>Header</p>
+    <div class="img-container">
+        <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="logo">
+    </div>
+</header>
