56d0851edcba9c2466e57d10fa7aa0254ae6376f
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-13 18:36:55 +0200

    [UPD] update and delete buttons

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,9 +5,19 @@
     <h2 class="mx-2 my-3">Elenco comics</h2>
     <ul>
         @foreach ($comics as $comic)
-        <li class="comic m-2">
-            <!--<img src="{{ $comic['thumb'] }}" alt="img">-->
-            <a class="gray" href="{{route('comics.show', $comic->id)}}">{{$comic['title']}}</a>
+        <li class="comic my-4 mx-2 border-bottom border-primary w-75 d-flex justify-content-between">
+            <div class="d-flex align-items-center">
+                <!--<img src="{{ $comic['thumb'] }}" alt="img">-->
+                <a class="gray" href="{{route('comics.show', $comic->id)}}">{{$comic['title']}}</a>
+            </div>
+            <div class="d-flex">
+                <button class="btn btn-outline-primary m-1">
+                    <i class="fa-solid fa-pencil text-primary"></i>
+                </button>
+                <button class="btn btn-outline-danger m-1">
+                    <i class="fa-solid fa-trash text-danger"></i>
+                </button>
+            </div>
         </li>
         @endforeach
     </ul>
