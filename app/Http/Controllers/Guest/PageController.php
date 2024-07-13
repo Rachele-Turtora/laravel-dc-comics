539c70541bc593d7e121bd0522739214920628f6
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-13 11:56:02 +0200

    [UPD] footer-top

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -22,6 +22,46 @@ class PageController extends Controller
             "shop"
         ];
 
-        return view('welcome', compact('header_list'));
+        $footer_lists = [
+            [
+                "Dc Comics",
+                "Characters",
+                "Comics",
+                "Movies",
+                "Tv",
+                "Games",
+                "Videos",
+                "News"
+            ],
+            [
+                "DC",
+                "Terms Of Use",
+                "Privacy policy (new)",
+                "AD Choices",
+                "Advertising",
+                "Jobs",
+                "Subscriptions",
+                "Talent Workshops",
+                "CPSC Certificates",
+                "Ratings",
+                "Shop Help",
+                "Contact Us"
+            ],
+            [
+                "Sites",
+                "DC",
+                "MAD Magazine",
+                "DC Kids",
+                "DC Universe",
+                "DC Power Visa"
+            ],
+            [
+                "Shop",
+                "Shop DC",
+                "Shop DC Collectibles",
+            ]
+        ];
+
+        return view('welcome', compact('header_list', 'footer_lists'));
     }
 }
--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,1 +1,22 @@
-<footer>Footer</footer>
+<footer>
+    <div class="footer-bg">
+        <div class="container">
+            <div class="footer-top">
+                <ul class=" footer-lists d-flex">
+                    @foreach ($footer_lists as $footer_list)
+                    <li class="m-2">
+                        <ul class="footer-list">
+                            @foreach ($footer_list as $item)
+                            <li>{{$item}}</li>
+                            @endforeach
+                        </ul>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="footer-bottom">
+
+            </div>
+        </div>
+    </div>
+</footer>
