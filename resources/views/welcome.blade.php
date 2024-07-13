9a1b81f3835b7a6c2130e649ade59d02a855997b
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-13 13:23:16 +0200

    [UPD] jumbo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('main')
-<h2>Ecco la pagina dei comics DC</h2>
-<a href="{{route('comics.index')}}">Vai all'elenco</a>
+<div class="jumbo">
+    <div class="overlay">
+        <div class="m-5">
+            <h1 class="text-light">DC Comics</h1>
+            <button class="text-uppercase fw-semibold my-3"><a href="{{route('comics.index')}}">Vai all'elenco</a></button>
+        </div>
+    </div>
+</div>
 @endsection
